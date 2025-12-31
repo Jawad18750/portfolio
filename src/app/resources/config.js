d5f29b9fc15068a4c7e9bc04dd00c91c9a73b547
Jawad Elmeladi <jawadelmeladi@gmail.com>
2026-01-01 01:01:08 +0200

    fix: update opacity values in config.js for gradient and dots
    - Changed gradient opacity from 0.7 to 1 for full visibility.
    - Adjusted dots opacity from 0.5 to 1 to enhance clarity.
    - Updated solid color option from 'color' to 'contrast' for better configuration clarity.

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -27,11 +27,11 @@ const effects = {
     mask: 'topRight',             // none | cursor | topLeft | topRight | bottomLeft | bottomRight
     gradient: {
         display: true,
-        opacity: 0.7           // 0 - 1
+        opacity: 1           // 0 - 1
     },
     dots: {
         display: true,
-        opacity: 0.5,           // 0 - 1
+        opacity: 1,           // 0 - 1
         size: '40'              // 2 | 4 | 8 | 12 | 16 | 20 | 24 | 32 | 40 | 48 | 56 | 634
     },
     lines: {
@@ -44,7 +44,7 @@ const style = {
     neutral:     'gray',         // sand | gray | slate
     brand:       'cyan',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
     accent:      'cyan',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    solid:       'color',     // color | contrast
+    solid:       'contrast',     // color | contrast
     solidStyle:  'plastic',         // flat | plastic
     border:      'rounded',      // rounded | playful | conservative
     surface:     'translucent',  // filled | translucent
